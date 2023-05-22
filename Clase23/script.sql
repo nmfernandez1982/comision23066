ff60ffb61ff01c0490a1995cfb6215fdcc902457
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2023-05-22 02:47:10 -0300

    Clase23

---
+++ Clase23/script.sql
@@ -0,0 +1,40 @@
+
+/*
+Base de datos:
+Una base de datos relacional define un conjunto de reglas que se deben respetar para almacenar la información que se necesite.
+
+
+Base de Datos:
+tablas-->entidades  
+vistas
+funciones
+Store procedure
+
+Lenguaje
+
+sql--> LENGUAJE DE PROGRAMACION. DML/DDL
+
+ORACLE
+SQL SERVER
+MYSQL/MariaDB
+PostgreSQL/SAP/Access/MongoDB
+
+DER--diagrama de entidad relacion.
+
+*/
+
+
+
+create database prueba66;
+
+use prueba66;
+
+drop database prueba66;
+
+
+
+
+
+
+
+
